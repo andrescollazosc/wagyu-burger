377cb1551b5759128ff5814eab049fd31265f153
Andres Collazos <andrescollazosc@gmail.com>
2021-09-23 21:39:39 -0500

    Added new deploy with logo

--- src/app/components/hero-navbar/hero-navbar.component.ts
+++ src/app/components/hero-navbar/hero-navbar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hero-navbar',
   templateUrl: './hero-navbar.component.html',
-  styleUrls: ['./hero-navbar.component.scss']
+  styleUrls: ['./hero-navbar.component.scss', './hero-navbar-logo.component.scss']
 })
 export class HeroNavbarComponent implements OnInit {
 
